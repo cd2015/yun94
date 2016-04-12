3e81bc00b961ea15309d369ef8b0a98a9310ec78
iamcastelli <sowed@cyberdude.com>
2016-04-12 17:58:28 +0300

    Create a simple structure of seeder
    Just a template, not finished yet

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,3 +14,27 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
     }
 }
+
+//BusinessDetailsSeeder
+//use App\News as News; // to use Eloquent Model
+
+class NewsTableSeeder extends Seeder
+{
+    public function run()
+    {
+        // clear table
+        News::truncate();
+        // add 1st row
+        News::create([
+            'url' => 'news-url',
+            'headline' => 'News Headline',
+            'story' => 'The Story',
+        ]);
+        // add 2nd row
+        News::create([
+            'url' => 'second-url',
+            'headline' => 'Second Headline',
+            'story' => 'Second Story',
+        ]);
+    }
+}
