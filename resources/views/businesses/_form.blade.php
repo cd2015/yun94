a603e727bd07136939f73e634241db9683b6878c
iamcastelli <sowed@cyberdude.com>
2016-04-30 20:51:23 +0300

    Create the `create` form and partial
    Create the add new business form. extract the edit form into a seperate
    form partial tobe shared with the create page

---
+++ resources/views/businesses/_form.blade.php
@@ -0,0 +1,46 @@
+{{-- Yuunga Forms Partial (Edit and Create) --}}
+<div class="form-group">
+    {!! Form::label('name', 'Name', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::text('name', null, ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('reference', 'Slug', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::text('reference', null, ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('logo', 'Logo', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::text('logo', null, ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('category', 'Category', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::text('category', null,  ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('tel_no', 'Mobile', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::number('tel_no', null,  ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('location', 'Location', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::text('location', null,  ['class' =>'form-control']) !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::label('about', 'About', ['class' =>'control-label  col-sm-2']) !!}
+    <div class=" col-sm-10">
+        {!! Form::textarea('about', null,  ['class' =>'form-control'])  !!}
+    </div>
+</div>
+<div class="form-group">
+    {!! Form::submit('Update Details', ['class' =>'btn btn-primary'])  !!}
+</div>
---
+++ resources/views/businesses/create.blade.php
@@ -0,0 +1,17 @@
+{{-- Yuunga Business Layout Module --}}
+@extends('main')
+
+@section('page-title', "Yuunga - Add a new Business")
+
+@section('main-header')
+    Add a new Business
+@stop
+
+@section('sidebar')
+@stop
+
+@section('main-content')
+    {!! Form::open(['route' => 'businesses_path', 'class' =>'form-horizontal']) !!}
+    @include('businesses._form')
+    {!! Form::close() !!}
+@stop
