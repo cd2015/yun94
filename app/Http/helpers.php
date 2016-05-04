1d9af2c93f3e888233b2a0e777d75c17faea9034
iamcastelli <sowed@cyberdude.com>
2016-05-04 20:37:53 +0300

    Add a helper function to generate Slug from the given Name
    This slug will be used in the business controller to generate SEO and
    USER friendly URLs in the address bar, as generated from the  name of
    the business

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -1,5 +1,15 @@
 <?php
 
+function slugify($str)
+{
+    $search = array('Ș', 'Ț', 'ş', 'ţ', 'Ş', 'Ţ', 'ș', 'ț', 'î', 'â', 'ă', 'Î', 'Â', 'Ă', 'ë', 'Ë');
+    $replace = array('s', 't', 's', 't', 's', 't', 's', 't', 'i', 'a', 'a', 'i', 'a', 'a', 'e', 'E');
+    $str = str_ireplace($search, $replace, strtolower(trim($str)));
+    $str = preg_replace('/[^\w\d\-\ ]/', '', $str);
+    $str = str_replace(' ', '-', $str);
+    return preg_replace('/\-{2,}/', '-', $str);
+}
+
 function delete_form($routeParams, $title = 'Delete Business')
 {
     $form = Form::open(['method' => 'DELETE', 'route' => $routeParams, 'class' => 'form-inline']);
