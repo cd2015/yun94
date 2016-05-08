ed8174706b1f42a895bf5c01249d2df13d2a92f7
iamcastelli <sowed@cyberdude.com>
2016-05-08 18:32:17 +0300

    Implement Routing methods on CategoryController
    First update `CategoryController` resource methods,
    Add a custom method to pull in `subcategories` (NB, a test method on the
    subcategories)
    Restructure the Router and add the CategoryController(CC)  resource and
    router for subcategories. (NB, subcategories route should be called
    before the resource otherwise it will not be cesses as the show method
    on the CC will over take it)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,85 +2,126 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use Illuminate\Http\Request;
-
+use Illuminate\View\View;
 use App\Http\Requests;
+use App\Http\Requests\CategoryRequest;
 
+/**
+ * Controller method the manages Categories
+ * Class CategoryController
+ * @package App\Http\Controllers
+ */
 class CategoryController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * @var Category
+     */
+    private $category;
+
+    /**
+     * CategoryController constructor.
+     * @param Category $selectedCategory
+     */
+    public function __construct(Category $selectedCategory)
+    {
+        $this->category = $selectedCategory;
+    }
+
+    /**
+     *Show a list of all Categories
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function index()
     {
-        //
+        $categories = $this->category->latest()->get();
+        return view('categories.index', compact('categories'));
     }
 
     /**
-     * Show the form for creating a new resource.
+     * Show the form for creating a new Category.
      *
-     * @return \Illuminate\Http\Response
+     * @return View
      */
     public function create()
     {
-        //
+        return view('categories.create');
     }
 
     /**
-     * Store a newly created resource in storage.
+     * Store a newly created Category in the database.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param Category $category
+     * @param CategoryRequest $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Category $category, CategoryRequest $request)
     {
-        //
+        $category->create($request->all());
+        return redirect()->route('categories_path');
+    }
+
+
+    /**
+     *Show details of a selected Category
+     *
+     * @param Category $category
+     * @return View
+     */
+    public function show(Category $category)
+    {
+        $subcategories = [['id' => '1', 'name' => 'Finance'], ['id' => '2', 'name' => 'Luxury']];
+        $subcategories = [];
+        return view('categories.show', compact('category', 'subcategories'));
     }
 
     /**
-     * Display the specified resource.
+     * Edit a selected Category
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return View
      */
-    public function show($id)
+    public function edit(Category $category)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return view('categories.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @param CategoryRequest $request
+     * @return \Illuminate\Http\RedirectResponse
      */
-    public function update(Request $request, $id)
+    public function update(Category $category, CategoryRequest $request)
     {
-        //
+        $category->update($request->input());
+        return redirect()->route('category_path', $category->id);
     }
 
     /**
-     * Remove the specified resource from storage.
+     * Remove the specified Category from the database.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param Category $category
+     * @return \Illuminate\Http\RedirectResponse
+     * @throws \Exception
      */
-    public function destroy($id)
+    public function destroy(Category $category)
     {
-        //
+        $category->delete();
+        return redirect()->route('categories_path');
+    }
+
+
+    /**
+     * @return string
+     */
+    public function subCategories()
+    {
+        $subcategories = [['id' => '1', 'name' => 'Finance'], ['id' => '2', 'name' => 'Luxury']];
+        //$subcategories = [];
+        return view('categories.subcategories', compact('subcategories'));
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\BusinessDetail;
+use App\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,24 +14,63 @@ use App\BusinessDetail;
 |
 */
 
-Route::bind('businesses', function ($reference) {
-    return BusinessDetail::whereReference($reference)->first();
-});
 Route::get('/', 'PagesController@index');
 Route::get('about', 'PagesController@about');
 
-Route::resource('businesses', 'BusinessController', [
-    'only' => [
-        'index', 'show', 'edit', 'update', 'create', 'store', 'destroy'
-    ],
-    'names' => [
-        'index' => 'businesses_path',
-        'show' => 'business_path',
-        'edit' => 'business_edit_path',
-        'create' => 'business_create_path',
-        'destroy' => 'business_delete_path',
-    ]
-]);
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
+
+
+/*
+|--------------------------------------------------------------------------
+| Business Route Resource
+|--------------------------------------------------------------------------
+|
+| This handles routing traffic for manipulation of businesses, like
+| creating a business, editing, listing or viewing one business.
+|
+*/
+Route::bind('businesses', function ($reference) {
+    return BusinessDetail::whereReference($reference)->first();
+});
+Route::resource('businesses', 'BusinessController', [
+    'only' => [
+        'index', 'create', 'store', 'show', 'edit', 'update', 'destroy'
+    ],
+    'names' => [
+        'index' => 'businesses_path',
+        'create' => 'business_create_path',
+        'show' => 'business_path',
+        'edit' => 'business_edit_path',
+        'destroy' => 'business_delete_path',
+    ]
+]);
+
+
+/*
+|--------------------------------------------------------------------------
+| Category Route Resource
+|--------------------------------------------------------------------------
+|
+| This handles routing traffic for manipulation of categories, like
+| creating a category, editing, listing or viewing one category.
+|
+*/
+Route::bind('categories', function ($id) {
+    return Category::whereId($id)->first();
+});
+Route::get('categories/subcategories', ['as' => 'subcategories_path', 'uses' => 'CategoryController@subCategories']);
+Route::resource('categories', 'CategoryController', [
+    'only' => [
+        'index', 'create', 'store', 'show', 'edit', 'update', 'destroy', 'showCategories'
+    ],
+    'names' => [
+        'index' => 'categories_path',
+        'create' => 'category_create_path',
+        'show' => 'category_path',
+        'edit' => 'category_edit_path',
+        'destroy' => 'category_delete_path',
+        'showCategories' => 'subcategories_path',
+    ]
+]);
