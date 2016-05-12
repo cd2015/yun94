e96b3e9bdc9e12d6e7219158039e356626611fb2
iamcastelli <sowed@cyberdude.com>
2016-05-13 02:43:59 +0300

    Add sub categories dynamic inputs

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,27 @@
 /*!
  * Yuunga Custom Logic
  */
+$(document).ready(function () {
+    var maxInput = 5, n = 1;
+    $(document).on('click', '.add-field-btn', function () {
+        var controlForm = $('#subcategories_fields'),
+            currentEntry = $(this).parents('.entry:first');
+        if (n < maxInput) {
+            var newEntry = $(currentEntry.clone()).appendTo(controlForm);
+            newEntry.find('input')
+                .attr("placeholder", "Add another Sub Category")
+                .attr("name", "employee[" + n + "][name]")
+                .val('');
+
+            controlForm.find('.entry:not(:last) .add-field-btn')
+                .removeClass('add-field-btn').addClass('remove-field-btn')
+                .removeClass('btn-success').addClass('btn-danger')
+                .html('<span class="glyphicon glyphicon-minus" title="Remove field"></span>');
+            n++;
+        }
+    }).on('click', '.remove-field-btn', function () {
+        $(this).parents('.entry:first').remove();
+        n--;
+        return false;
+    });
+});
--- resources/views/categories/_form.blade.php
+++ resources/views/categories/_form.blade.php
@@ -7,6 +7,23 @@
         {!! $errors->first('name', '<span class="help-block">:message</span>') !!}
     </div>
 </div>
+
+
+<div id="subcategories_fields" class="form-group {{$errors->has('subcategories')?'has-error':''}}">
+    <label>Add SubCategories</label>
+
+    <div class="entry col-sm-10">
+        <div class="input-group" style="margin-bottom: 8px;">
+            <input class="form-control" name="employee[0][name]" type="text" placeholder="Enter a Sub Category"/>
+
+            <div class="input-group-btn">
+                <div class="btn btn-success add-field-btn" type="button">
+                    <span class="glyphicon glyphicon-plus"></span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 <div class="form-group">
     {!! Form::submit($SubmitButtonText, ['class' =>'btn '. $SubmitButtonClass])  !!}
 </div>
