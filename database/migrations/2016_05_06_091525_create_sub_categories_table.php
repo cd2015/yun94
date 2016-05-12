fc5ad7764ac57e3056b979d024003361021bbfa7
iamcastelli <sowed@cyberdude.com>
2016-05-12 11:30:57 +0300

    Fix `id` errror in the sub_category migration
    Replace the `user_id` foreign key constraint with `category_id`
    constraint.

--- database/migrations/2016_05_06_091525_create_sub_categories_table.php
+++ database/migrations/2016_05_06_091525_create_sub_categories_table.php
@@ -19,7 +19,7 @@ class CreateSubCategoriesTable extends Migration
             $table->integer('category_id')->unsigned();
             $table->timestamps();
 
-            $table->foreign('user_id')
+            $table->foreign('category_id')
                 ->references('id')
                 ->on('categories')
                 ->onDelete('cascade');
