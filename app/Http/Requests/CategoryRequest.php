4fd923c1a0371cb9e30c3048a36193eef4f64764
iamcastelli <sowed@cyberdude.com>
2016-05-14 08:31:35 +0300

    Update Category rule to restrict to strings

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|min:3|max:30|unique:categories',
+            'name' => 'required|min:3|max:30|unique:categories|string',
         ];
     }
 }
